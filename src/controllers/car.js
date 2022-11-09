dd78a52dd5e2dd6142d5ff4fca1194be15635706
skereboi <mj.jesusj@gmail.com>
2022-11-08 23:58:26 -0600

    Car -> Routers, Controllers funcionando

---
+++ src/controllers/car.js
@@ -0,0 +1,91 @@
+const prismaClient = require('@prisma/client');
+const createHttpError = require('http-errors');
+
+const prisma = new prismaClient.PrismaClient();
+
+// Get ALL
+const getAll = async (_req, res, next) => {
+    try {
+        const cars = await prisma.car.findMany();
+
+        return res.json(cars);
+    } catch (error) {
+        return next(error);
+    }
+};
+
+// Get by ID
+const get = async (req, res, next) => {
+    try {
+        const id = Number(req.params.id);
+
+        const car = await prisma.car.findUnique({
+            where: { id },
+        });
+
+        if (car == null) throw createHttpError[404]('No car found 👉🏼👈🏼 💀');
+
+        return res.json(car);
+    } catch (error) {
+        return next(error);
+    }
+};
+
+// Create Car -- Faltan Endpoints de modelos
+const create = async (req, res, next) => {
+    try {
+        const { currentKilometers, image, description, circulationCardValidity, modelId } = req.body;
+
+        const result = await prisma.car.create({
+            data: {
+                currentKilometers,
+                image,
+                description,
+                circulationCardValidity: new Date(circulationCardValidity),
+                model: {
+                    connect: {
+                        id: modelId,
+                    },
+                },
+            },
+        });
+
+        return res.json(result);
+    } catch (error) {
+        return next(error);
+    }
+};
+
+const update = async (req, res, next) => {
+    try {
+        const { currentKilometers, image, description, circulationCardValidity } = req.body;
+        const id = Number(req.params.id);
+  
+      const car = await prisma.car.findUnique({
+        where: { id },
+      });
+  
+      if (car == null) throw createHttpError[404]('No car found 👉🏼👈🏼');
+  
+      const response = await prisma.car.update({
+        where: { id },
+        data: {
+            currentKilometers,
+            image,
+            description,
+            circulationCardValidity: new Date(circulationCardValidity),
+        },
+      });
+  
+      return res.json(response);
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+module.exports = {
+    getAll,
+    get,
+    create,
+    update,
+};
