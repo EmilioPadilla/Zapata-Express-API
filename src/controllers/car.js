3ad86b1a3b63b72997a5a93810e85101a71d4acc
Luis Rodrigo Montúfar Pérez <A01273078@itesm.mx>
2022-11-12 14:41:43 -0600

    Update car.js

--- src/controllers/car.js
+++ src/controllers/car.js
@@ -29,7 +29,7 @@ const get = async (req, res, next) => {
     }
 };
 
-const getByUserId = async (req, res, next) => {
+const getByClientId = async (req, res, next) => {
     try {
         const id = Number(req.params.id);
 
